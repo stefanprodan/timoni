367c7493e1dbd5198ebf210c69340302ce6125d1
Stefan Prodan <stefan.prodan@gmail.com>
2023-02-13 22:09:03 +0200

    Add build command
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

---
+++ cmd/timoni/build.go
@@ -0,0 +1,145 @@
+/*
+Copyright 2023 Stefan Prodan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"cuelang.org/go/cue/cuecontext"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/yaml"
+)
+
+var buildCmd = &cobra.Command{
+	Use:     "build [NAME] [MODULE]",
+	Aliases: []string{"template"},
+	Short:   "Build the module locally and print the resulting Kubernetes resources.",
+	Example: `  # Build a local module with the default values
+  timoni build app ./path/to/module --output yaml
+
+  # Build a module with custom values by merging them in the specified order. 
+  timoni build app ./path/to/module \
+  --values ./values-1.cue \
+  --values ./values-2.cue
+`,
+	RunE: runBuildCmd,
+}
+
+type buildFlags struct {
+	name        string
+	module      string
+	pkg         string
+	valuesFiles []string
+	output      string
+}
+
+var buildArgs buildFlags
+
+func init() {
+	buildCmd.Flags().StringVarP(&buildArgs.pkg, "package", "p", "main",
+		"The name of the package containing the instance values and resources.")
+	buildCmd.Flags().StringSliceVarP(&buildArgs.valuesFiles, "values", "f", nil, "local path to values.cue files")
+	buildCmd.Flags().StringVarP(&buildArgs.output, "output", "o", "yaml",
+		"the format in which the Kubernetes resources should be printed, can be 'json' or 'yaml'")
+	rootCmd.AddCommand(buildCmd)
+}
+
+func runBuildCmd(cmd *cobra.Command, args []string) error {
+	if len(args) < 2 {
+		return fmt.Errorf("name and module are required")
+	}
+
+	buildArgs.name = args[0]
+	buildArgs.module = args[1]
+
+	ctx := cuecontext.New()
+
+	if _, err := os.Stat(buildArgs.module); err != nil {
+		return fmt.Errorf("package not found at path %s", buildArgs.module)
+	}
+
+	logger.Println("building", buildArgs.module)
+
+	tmpDir, err := os.MkdirTemp("", "timoni")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	modulePath := filepath.Join(tmpDir, "module")
+	err = copyModule(buildArgs.module, modulePath)
+	if err != nil {
+		return err
+	}
+
+	builder := NewBuilder(ctx, modulePath, buildArgs.pkg)
+
+	if len(buildArgs.valuesFiles) > 0 {
+		err = builder.MergeValuesFile(buildArgs.valuesFiles)
+		if err != nil {
+			return err
+		}
+	}
+
+	buildResult, err := builder.Build()
+	if err != nil {
+		return fmt.Errorf("failed to build instance, error: %w", err)
+	}
+
+	kubeRes, err := builder.SelectResources(buildResult)
+	if err != nil {
+		return fmt.Errorf("failed to extract resouces, error: %w", err)
+	}
+	switch buildArgs.output {
+	case "yaml":
+		var sb strings.Builder
+		for _, obj := range kubeRes {
+			data, err := yaml.Marshal(obj)
+			if err != nil {
+				return fmt.Errorf("failed to convert resouces, error: %w", err)
+			}
+			sb.Write(data)
+			sb.WriteString("---\n")
+		}
+		_, err = cmd.OutOrStdout().Write([]byte(sb.String()))
+	case "json":
+		list := struct {
+			ApiVersion string                       `json:"apiVersion,omitempty"`
+			Kind       string                       `json:"kind,omitempty"`
+			Items      []*unstructured.Unstructured `json:"items,omitempty"`
+		}{
+			ApiVersion: "v1",
+			Kind:       "List",
+			Items:      kubeRes,
+		}
+
+		b, err := json.MarshalIndent(list, "", "    ")
+		if err != nil {
+			return fmt.Errorf("failed to convert resouces, error: %w", err)
+		}
+		_, err = cmd.OutOrStdout().Write(b)
+	default:
+		return fmt.Errorf("unkown --output=%s, can be yaml or json", buildArgs.output)
+	}
+
+	return err
+}
---
+++ cmd/timoni/builder.go
@@ -0,0 +1,289 @@
+/*
+Copyright 2023 Stefan Prodan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/load"
+	"cuelang.org/go/encoding/yaml"
+	"github.com/fluxcd/pkg/runtime/transform"
+	"github.com/fluxcd/pkg/ssa"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	defaultPackage       = "main"
+	defaultValuesName    = "values"
+	defaultValuesFile    = "values.cue"
+	defaultResourcesName = "resources"
+)
+
+// Builder complies CUE definitions to Kubernetes objects.
+type Builder struct {
+	ctx        *cue.Context
+	moduleRoot string
+	pkgName    string
+	pkgPath    string
+}
+
+// NewBuilder creates a Builder for the given module and package.
+func NewBuilder(ctx *cue.Context, moduleRoot, pkgName string) *Builder {
+	if ctx == nil {
+		ctx = cuecontext.New()
+	}
+	b := &Builder{
+		ctx:        ctx,
+		moduleRoot: moduleRoot,
+		pkgName:    pkgName,
+		pkgPath:    moduleRoot,
+	}
+	if pkgName != defaultPackage {
+		b.pkgPath = filepath.Join(moduleRoot, pkgName)
+	}
+	return b
+}
+
+// MergeValuesFile merges the given values overlays into values.cue.
+func (b *Builder) MergeValuesFile(overlays []string) error {
+	vFinalMap := make(map[string]interface{})
+	defaultFile := filepath.Join(b.pkgPath, defaultValuesFile)
+
+	vDefaultMap, err := b.valuesFromFile(defaultFile)
+	if err != nil {
+		return fmt.Errorf("invalid values in %s, error: %w", defaultFile, err)
+	}
+	vFinalMap = transform.MergeMaps(vFinalMap, vDefaultMap)
+
+	for _, overlay := range overlays {
+		vOverlayMap, err := b.valuesFromFile(overlay)
+		if err != nil {
+			return fmt.Errorf("invalid values in %s, error: %w", overlay, err)
+		}
+
+		vFinalMap = transform.MergeMaps(vFinalMap, vOverlayMap)
+	}
+
+	vFinalData, err := json.Marshal(vFinalMap)
+	if err != nil {
+		return fmt.Errorf("mergeing values failed, error: %w", err)
+	}
+
+	vFinal := b.ctx.CompileString(string(vFinalData))
+	if vFinal.Err() != nil {
+		return fmt.Errorf("compiling final alues failed, error: %w", vFinal.Err())
+	}
+
+	cueGen := fmt.Sprintf("package %s\n%s: %v", b.pkgName, defaultValuesName, vFinal)
+
+	//logger.Println(string(vFinalData))
+	//logger.Println(cueGen)
+
+	if err := os.MkdirAll(b.moduleRoot, os.ModePerm); err != nil {
+		return err
+	}
+
+	return os.WriteFile(defaultFile, []byte(cueGen), 0644)
+}
+
+// Build builds a CUE instances for the specified package and returns the CUE value.
+func (b *Builder) Build() (cue.Value, error) {
+	var value cue.Value
+	cfg := &load.Config{
+		ModuleRoot: b.moduleRoot,
+		Package:    b.pkgName,
+		Dir:        b.pkgPath,
+		DataFiles:  true,
+		Tags:       []string{},
+		TagVars:    map[string]load.TagVar{},
+	}
+
+	ix := load.Instances([]string{}, cfg)
+	if len(ix) == 0 {
+		return value, fmt.Errorf("no instances found")
+	}
+
+	inst := ix[0]
+	if inst.Err != nil {
+		return value, fmt.Errorf("instance error: %w", inst.Err)
+	}
+
+	v := b.ctx.BuildInstance(inst)
+	if v.Err() != nil {
+		return value, v.Err()
+	}
+
+	return v, nil
+}
+
+// SelectResources coverts the CUE value to Kubernetes unstructured objects
+func (b *Builder) SelectResources(value cue.Value) ([]*unstructured.Unstructured, error) {
+	expr := value.LookupPath(cue.ParsePath(defaultResourcesName))
+	if expr.Err() != nil {
+		return nil, fmt.Errorf("lookup resouces failed, error: %w", expr.Err())
+	}
+
+	switch expr.Kind() {
+	case cue.ListKind:
+		//data, err := expr.MarshalJSON()
+		//if err != nil {
+		//	return nil, err
+		//}
+		items, err := expr.List()
+		if err != nil {
+			return nil, fmt.Errorf("listing resouces failed, error: %w", err)
+		}
+
+		data, err := yaml.EncodeStream(items)
+		if err != nil {
+			return nil, fmt.Errorf("encoding resouces to YAML failed, error: %w", err)
+		}
+		return ssa.ReadObjects(bytes.NewReader(data))
+	default:
+		return nil, fmt.Errorf("resouces are not of type cue.ListKind, got %v", value.Kind())
+	}
+}
+
+func (b *Builder) valuesFromFile(filePath string) (map[string]interface{}, error) {
+	vData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	vObj := b.ctx.CompileBytes(vData)
+	if vObj.Err() != nil {
+		return nil, vObj.Err()
+	}
+
+	vValue := vObj.LookupPath(cue.ParsePath(defaultValuesName))
+	if vValue.Err() != nil {
+		return nil, vObj.Err()
+	}
+
+	vJSON, err := vValue.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+
+	vMap := make(map[string]interface{})
+	err = json.Unmarshal(vJSON, &vMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return vMap, nil
+}
+
+func copyModuleFile(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func copyModule(src string, dst string) (err error) {
+	src = filepath.Clean(src)
+	dst = filepath.Clean(dst)
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !si.IsDir() {
+		return fmt.Errorf("source is not a directory")
+	}
+
+	_, err = os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+	if err == nil {
+		return fmt.Errorf("destination already exists")
+	}
+
+	err = os.MkdirAll(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	entries, err := os.ReadDir(src)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			err = copyModule(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		} else {
+			if fi, fiErr := entry.Info(); fiErr != nil || !fi.Mode().IsRegular() {
+				return
+			}
+
+			err = copyModuleFile(srcPath, dstPath)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
