b963ca663a55b4327d779c3b04038ae585549399
Stefan Prodan <stefan.prodan@gmail.com>
2023-11-19 01:11:13 +0200

    Add multi-cluster support to `bundle status` cmd
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- cmd/timoni/bundle_status.go
+++ cmd/timoni/bundle_status.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"maps"
 
 	"cuelang.org/go/cue/cuecontext"
 	"github.com/fluxcd/cli-utils/pkg/kstatus/status"
@@ -75,65 +76,96 @@ func runBundleStatusCmd(cmd *cobra.Command, args []string) error {
 		bundleStatusArgs.name = args[0]
 	}
 
-	rm, err := runtime.NewResourceManager(kubeconfigArgs)
+	runtimeValues := make(map[string]string)
+
+	if bundleArgs.runtimeFromEnv {
+		maps.Copy(runtimeValues, engine.GetEnv())
+	}
+
+	rt, err := buildRuntime(bundleArgs.runtimeFiles)
 	if err != nil {
 		return err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), rootArgs.timeout)
-	defer cancel()
-
-	sm := runtime.NewStorageManager(rm)
-	instances, err := sm.List(ctx, "", bundleStatusArgs.name)
-	if err != nil {
-		return err
+	clusters := rt.SelectClusters(bundleArgs.runtimeCluster, bundleArgs.runtimeClusterGroup)
+	if len(clusters) == 0 {
+		return fmt.Errorf("no cluster found")
 	}
 
-	if len(instances) == 0 {
-		return fmt.Errorf("no instances found in bundle")
-	}
+	failed := false
+	for _, cluster := range clusters {
+		kubeconfigArgs.Context = &cluster.KubeContext
 
-	for _, instance := range instances {
-		log := LoggerBundleInstance(ctx, bundleStatusArgs.name, apiv1.RuntimeDefaultName, instance.Name)
-
-		log.Info(fmt.Sprintf("last applied %s",
-			colorizeSubject(instance.LastTransitionTime)))
-		log.Info(fmt.Sprintf("module %s",
-			colorizeSubject(instance.Module.Repository+":"+instance.Module.Version)))
-		log.Info(fmt.Sprintf("digest %s",
-			colorizeSubject(instance.Module.Digest)))
-
-		for _, image := range instance.Images {
-			log.Info(fmt.Sprintf("container image %s",
-				colorizeSubject(image)))
-		}
-
-		im := runtime.InstanceManager{Instance: apiv1.Instance{Inventory: instance.Inventory}}
-
-		objects, err := im.ListObjects()
+		rm, err := runtime.NewResourceManager(kubeconfigArgs)
 		if err != nil {
 			return err
 		}
 
-		for _, obj := range objects {
-			err = rm.Client().Get(ctx, client.ObjectKeyFromObject(obj), obj)
+		ctx, cancel := context.WithTimeout(context.Background(), rootArgs.timeout)
+		defer cancel()
+
+		sm := runtime.NewStorageManager(rm)
+		instances, err := sm.List(ctx, "", bundleStatusArgs.name)
+		if err != nil {
+			return err
+		}
+
+		log := LoggerBundle(ctx, bundleStatusArgs.name, cluster.Name)
+
+		if len(instances) == 0 {
+			log.Error(nil, "no instances found in bundle")
+			failed = true
+			continue
+		}
+
+		for _, instance := range instances {
+			log := LoggerBundleInstance(ctx, bundleStatusArgs.name, cluster.Name, instance.Name)
+
+			log.Info(fmt.Sprintf("last applied %s",
+				colorizeSubject(instance.LastTransitionTime)))
+			log.Info(fmt.Sprintf("module %s",
+				colorizeSubject(instance.Module.Repository+":"+instance.Module.Version)))
+			log.Info(fmt.Sprintf("digest %s",
+				colorizeSubject(instance.Module.Digest)))
+
+			for _, image := range instance.Images {
+				log.Info(fmt.Sprintf("container image %s",
+					colorizeSubject(image)))
+			}
+
+			im := runtime.InstanceManager{Instance: apiv1.Instance{Inventory: instance.Inventory}}
+
+			objects, err := im.ListObjects()
 			if err != nil {
-				if apierrors.IsNotFound(err) {
-					log.Error(err, colorizeJoin(obj, errors.New("NotFound")))
+				return err
+			}
+
+			for _, obj := range objects {
+				err = rm.Client().Get(ctx, client.ObjectKeyFromObject(obj), obj)
+				if err != nil {
+					if apierrors.IsNotFound(err) {
+						log.Error(err, colorizeJoin(obj, errors.New("NotFound")))
+						failed = true
+
+						continue
+					}
+					log.Error(err, colorizeJoin(obj, errors.New("Unknown")))
+					failed = true
 					continue
 				}
-				log.Error(err, colorizeJoin(obj, errors.New("Unknown")))
-				continue
-			}
 
-			res, err := status.Compute(obj)
-			if err != nil {
-				log.Error(err, colorizeJoin(obj, errors.New("Failed")))
-				continue
+				res, err := status.Compute(obj)
+				if err != nil {
+					log.Error(err, colorizeJoin(obj, errors.New("Failed")))
+					failed = true
+					continue
+				}
+				log.Info(colorizeJoin(obj, res.Status, "-", res.Message))
 			}
-			log.Info(colorizeJoin(obj, res.Status, "-", res.Message))
 		}
 	}
-
+	if failed {
+		return fmt.Errorf("completed with errors")
+	}
 	return nil
 }
